ca7d9252a9ac840a1d87b2bbfb36b7b418d51206
johnm <johnmiko4@gmail.com>
2026-02-10 11:47:41 -0700

    move db to be inside of package

--- cribbage/constants.py
+++ cribbage/constants.py
@@ -7,7 +7,7 @@ load_dotenv(override=True)
 DB_PATH = os.getenv("DB_PATH", "C:/Users/johnm/ccode/crib_engine/crib_cache.db")
 HAND_CRIB_DB_PATH = os.getenv(
     "HAND_CRIB_DB_PATH",
-    str((Path(__file__).resolve().parents[1] / "data" / "hand_crib_stats.sqlite")),
+    str((Path(__file__).resolve().parent / "data" / "hand_crib_stats.sqlite")),
 )
 
 if DB_PATH is None:
